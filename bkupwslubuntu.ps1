ba5a504af7f427f0802029fadc5438cde7a83998
Mark Ryan <63432380+TerminatedProcess@users.noreply.github.com>
2023-11-06 21:20:13 -0500

    Refactored the code to move the Docker-Stop function to a separate section and removed duplicate code.

--- bkupwslubuntu.ps1
+++ bkupwslubuntu.ps1
@@ -145,6 +145,31 @@ function Check-Process {
     return $process
 }
 
+function Docker-Stop {
+    # Stop Docker Desktop so it does not interfere with backup process.
+    Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Attempting to stop Docker Desktop..."
+
+    # Stop Docker Desktop service if it exists and is running
+    $dockerService = Get-Service -Name 'com.docker.service' -ErrorAction SilentlyContinue
+    if ($dockerService -and $dockerService.Status -eq 'Running') {
+        Stop-Service $dockerService.Name -Force
+        Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Docker service stopped."
+    } else {
+        Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Docker service not running or not found."
+    }
+
+    # Stop Docker Desktop processes
+    $dockerProcesses = Get-Process -Name 'Docker*' -ErrorAction SilentlyContinue
+    if ($dockerProcesses) {
+        $dockerProcesses | Stop-Process -Force
+        Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Docker processes stopped."
+    } else {
+        Write-Output "$((Get-Date).ToString("HH:mm:ss")) - No Docker processes found."
+    }
+
+    Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Docker Desktop should now be stopped."
+}
+
 function Test-Ini {
     # Create a new instance of the IniFile class
     $iniFile = [IniFile]::new()
@@ -237,28 +262,4 @@ if (-Not (Test-Path $outFile)) {
 } else {
     Write-Host "Backup completed successfully."
     explorer $destDir
-}
-
-function Docker-Stop {
-    Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Attempting to stop Docker Desktop..."
-
-    # Stop Docker Desktop service if it exists and is running
-    $dockerService = Get-Service -Name 'com.docker.service' -ErrorAction SilentlyContinue
-    if ($dockerService -and $dockerService.Status -eq 'Running') {
-        Stop-Service $dockerService.Name -Force
-        Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Docker service stopped."
-    } else {
-        Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Docker service not running or not found."
-    }
-
-    # Stop Docker Desktop processes
-    $dockerProcesses = Get-Process -Name 'Docker*' -ErrorAction SilentlyContinue
-    if ($dockerProcesses) {
-        $dockerProcesses | Stop-Process -Force
-        Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Docker processes stopped."
-    } else {
-        Write-Output "$((Get-Date).ToString("HH:mm:ss")) - No Docker processes found."
-    }
-
-    Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Docker Desktop should now be stopped."
 }
