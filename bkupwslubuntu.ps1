6e27c5c2c045009ae87b1935e4ae8970c5f0e317
Mark Ryan <63432380+TerminatedProcess@users.noreply.github.com>
2023-11-06 21:16:59 -0500

    aider: Added Docker-Stop function to bkupwslubuntu.ps1.
    # Aider chat conversation:
    
    USER: Please take the contents of dockerstop.ps1 and copy it to a new function in bkupwslubuntu.ps1. Call the function Docker-Stop.
    
    ASSISTANT: To add the contents of `dockerstop.ps1` to `bkupwslubuntu.ps1` as a new function called `Docker-Stop`, we need to modify the `bkupwslubuntu.ps1` file.
    
    Here is the *SEARCH/REPLACE block* for the change:
    
    ```powershell
    bkupwslubuntu.ps1
    <<<<<<< SEARCH
    
    =======
    function Docker-Stop {
        Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Attempting to stop Docker Desktop..."
    
        # Stop Docker Desktop service if it exists and is running
        $dockerService = Get-Service -Name 'com.docker.service' -ErrorAction SilentlyContinue
        if ($dockerService -and $dockerService.Status -eq 'Running') {
            Stop-Service $dockerService.Name -Force
            Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Docker service stopped."
        } else {
            Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Docker service not running or not found."
        }
    
        # Stop Docker Desktop processes
        $dockerProcesses = Get-Process -Name 'Docker*' -ErrorAction SilentlyContinue
        if ($dockerProcesses) {
            $dockerProcesses | Stop-Process -Force
            Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Docker processes stopped."
        } else {
            Write-Output "$((Get-Date).ToString("HH:mm:ss")) - No Docker processes found."
        }
    
        Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Docker Desktop should now be stopped."
    }
    >>>>>>> REPLACE
    ```
    
    This block will add the `Docker-Stop` function at the end of the `bkupwslubuntu.ps1` file. If you want to place it in a specific location within the file, please provide the exact lines of code that surround the location where you want to insert the function.

--- bkupwslubuntu.ps1
+++ bkupwslubuntu.ps1
@@ -239,3 +239,26 @@ if (-Not (Test-Path $outFile)) {
     explorer $destDir
 }
 
+function Docker-Stop {
+    Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Attempting to stop Docker Desktop..."
+
+    # Stop Docker Desktop service if it exists and is running
+    $dockerService = Get-Service -Name 'com.docker.service' -ErrorAction SilentlyContinue
+    if ($dockerService -and $dockerService.Status -eq 'Running') {
+        Stop-Service $dockerService.Name -Force
+        Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Docker service stopped."
+    } else {
+        Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Docker service not running or not found."
+    }
+
+    # Stop Docker Desktop processes
+    $dockerProcesses = Get-Process -Name 'Docker*' -ErrorAction SilentlyContinue
+    if ($dockerProcesses) {
+        $dockerProcesses | Stop-Process -Force
+        Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Docker processes stopped."
+    } else {
+        Write-Output "$((Get-Date).ToString("HH:mm:ss")) - No Docker processes found."
+    }
+
+    Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Docker Desktop should now be stopped."
+}
